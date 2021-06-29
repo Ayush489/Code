6e8f8ab0939bb59288adbf1690f6dd4d0d08147d
Ayush489 <86658135+Ayush489@users.noreply.github.com>
2021-06-29 16:04:48 +0530

    Update Sorting 2.cpp

--- Sorting 2.cpp
+++ Sorting 2.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 using namespace std;
 
-// Function to print an array
 void printArray(int array[], int size) {
   for (int i = 0; i < size; i++) {
     cout << array[i] << " ";
@@ -16,13 +15,29 @@ void insertionSort(int array[], int size) {
     int key = array[step];
     int j = step - 1;
 
-    // Compare key with each element on the left of it until an element smaller than
-    // it is found.
-    // For descending order, change key<array[j] to key>array[j].
     while (key < array[j] && j >= 0) {
       array[j + 1] = array[j];
       --j;
     }
     array[j + 1] = key;
   }
+}
+
+int main() 
+{ 
+   int i,n;
+   cout<<"Enter total number of elements:"<<"\n";
+   cin>>n;
+  
+   int *arr = new int[n];
+   cout<<"Enter "<<n<<" elements"<<endl;
+   for(i = 0;i<n;i++) {
+      cin>>arr[i];
+   }
+   cout<<endl;
+    insertionSort(arr, n); 
+    cout<<"Sorted array: \n"; 
+    printArray(arr, n); 
+    delete[] arr;
+    return 0; 
 }
